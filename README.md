098ec4827989b1984af9b1a073aac6821e1aa00b
Jurgen Segaert <jsegaert@yahoo.com>
2020-04-16 22:42:22 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # iocage-plugin-deluge-pip
+Artifact file(s) for Deluge iocage plugin
+
+This plugin will install Deluge and its WebUI from the Python Package Index (PyPI)
+
+## To install this Plugin
+Download the plugin manifest file to your local file system.
+```
+fetch https://raw.githubusercontent.com/jsegaert/iocage-my-plugins/master/deluge-pip.json
+```
+Install the plugin.  Adjust the network settings as needed.
+```
+iocage fetch -P deluge-pip.json -n deluge
+```
+
+
+
---
+++ post_install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Create media user
+pw useradd -n deluge -u 8675309 -m -c "Deluge BitTorrent Client" -s /usr/sbin/nologin -w no
+mkdir -p /home/deluge/.config/deluge     
+chown -R deluge:deluge /home/deluge/
+
+# Configure the services
+sysrc -f /etc/rc.conf deluged_enable="YES"
+sysrc -f /etc/rc.conf deluged_user="deluge"
+
+sysrc -f /etc/rc.conf deluge_web_enable="YES"
+sysrc -f /etc/rc.conf deluge_web_user="deluge"
+
+echo "Deluge Web UI Password: deluge" > /root/PLUGIN_INFO
+echo "To change, login and you will be prompted." >> /root/PLUGIN_INFO
+
+echo "Deluge Web UI Password: deluge" > /root/PLUGIN_INFO
+echo "To change, login and you will be prompted." >> /root/PLUGIN_INFO
+
+# Start the services
+service deluged start
+service deluge_web start
