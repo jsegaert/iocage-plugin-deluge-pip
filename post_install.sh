0b5cf0782e1706af5aa9e0df0b364845dc954eef
Jurgen Segaert <jsegaert@yahoo.com>
2020-04-19 13:25:37 -0700

    Add patch for ticket 3278

--- post_install.sh
+++ post_install.sh
@@ -8,9 +8,12 @@ chown -R deluge:deluge /home/deluge/
 mkdir /Downloads
 chown deluge:deluge /Downloads
 
-# Install the system
+# Install the packages
 pip install --upgrade pip
 pip install deluge
+
+# Install fix for https://dev.deluge-torrent.org/ticket/3278
+patch -F 0 /usr/local/lib/python3.7/site-packages/deluge/argparserbase.py /usr/local/etc/deluge_changeset_1b4ac88ce.patch
 
 # Configure the services
 sysrc -f /etc/rc.conf deluged_enable="YES"
@@ -19,12 +22,11 @@ sysrc -f /etc/rc.conf deluged_user="deluge"
 sysrc -f /etc/rc.conf deluge_web_enable="YES"
 sysrc -f /etc/rc.conf deluge_web_user="deluge"
 
-echo "Deluge Web UI Password: deluge" > /root/PLUGIN_INFO
+echo "The initial password for the WebUI is: deluge" > /root/PLUGIN_INFO
 echo "To change, login and you will be prompted." >> /root/PLUGIN_INFO
-
-echo "Deluge Web UI Password: deluge" > /root/PLUGIN_INFO
-echo "To change, login and you will be prompted." >> /root/PLUGIN_INFO
+echo "The default download directory is: /Downloads" >> /root/PLUGIN_INFO
 
 # Start the services
 service deluged start
 service deluge_web start
+
